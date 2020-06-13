b0a58429c4dc1c74c5f8532e1f0b7b09705ab4b8
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-06-13 13:19:00 -0700

    first attempt at the searching.py... both test are failing. Will be working to figure this out

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,33 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return 1
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    lowest = 0
+    highest = (len(arr) -1)
+
+    found = False
 
     # Your code here
+    while lowest <= highest and not found:
+        middle = (lowest + highest) // 2
 
+        if arr[middle] == target:
+            found = True
+
+        else:
+            if target < arr[middle]:
+                highest = middle - 1
+            # searching the bottom half
+            else:
+                lowest = middle +1
+
+    return found
 
     return -1  # not found
